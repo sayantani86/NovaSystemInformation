d36db69701793b661609edb50fba8e67d81aaefd
EC2 Default User <ec2-user@ip-172-30-2-104.ec2.internal>
2025-07-13 08:25:20 +0000

    Add date min max for multi and single wells

--- app/scripts/getWellDetails.sql
+++ app/scripts/getWellDetails.sql
@@ -1,6 +1,6 @@
-DROP FUNCTION getWellDetails(refid text);
+DROP FUNCTION getWellDetails(refid numeric);
 
-CREATE OR REPLACE FUNCTION getWellDetails(refid text) 
+CREATE OR REPLACE FUNCTION getWellDetails(refid numeric) 
  RETURNS TABLE (
 	w_description varchar,
 	w_county varchar,
@@ -16,36 +16,48 @@ CREATE OR REPLACE FUNCTION getWellDetails(refid text)
 	w_gl1 varchar,
 	w_gl1_range varchar,
 	w_gl2 varchar,
-	w_gl2_range varchar
+	w_gl2_range varchar,
+	min_entry_date date,
+	max_entry_date date,
+	calendar_min date,
+	calendar_max date
 ) AS 
 $$
 BEGIN
-	RAISE NOTICE 'RefId = %', quote_literal(refid);
+	RAISE NOTICE 'RefId = %', refid;
 
 	RETURN QUERY 
-		SELECT description,
-			county, 
-			state, 
-			pv_wc_id::numeric,
-			mth_1st_activity::date,
-			firstproductiondate::date,
-			shl_x, 
-			shl_y,
-			longitude::varchar,
-			latitude::varchar,
-			foreman, 
-			gl1, 
-			"GL1 Range"::varchar, 
-			gl2, 
-			"GL2 Range"::varchar
-		 FROM 
-			(
-				SELECT * FROM maps.wells_in_3_datasources UNION
-                        	SELECT * FROM maps.baytex_wells_not_in_master_in_quorum UNION
-                        	SELECT * FROM maps.master_wells_in_baytex_not_in_quorum UNION
-                        	SELECT * FROM maps.master_wells_not_in_baytex_in_quorum
-			) a
-		WHERE a.pv_wc_id::numeric = getWellDetails.refid::numeric;
+		SELECT b.*, c.min_entry_date,c.max_entry_date, d.calendar_min, d.calendar_max FROM (
+			SELECT description,
+				county, 
+				state, 
+				pv_wc_id::numeric,
+				mth_1st_activity::date,
+				firstproductiondate::date,
+				shl_x, 
+				shl_y,
+				longitude::varchar,
+				latitude::varchar,
+				foreman, 
+				gl1, 
+				"GL1 Range"::varchar, 
+				gl2, 
+				"GL2 Range"::varchar
+			 FROM 
+				(
+					SELECT * FROM maps.wells_in_3_datasources UNION
+					SELECT * FROM maps.master_wells_in_quorum_only UNION
+					SELECT * FROM maps.master_wells_unmatched_with_baytex UNION
+					SELECT * FROM maps.master_wells_with_geom_only UNION
+					SELECT * FROM maps.others
+				) a
+			WHERE 
+				a.pv_wc_id::numeric = getWellDetails.refid::numeric
+		) b LEFT JOIN quorum_range_partitioned_jul10_hist_min_max_date c 
+		ON 
+			b.pv_wc_id::numeric = c.refid_out::numeric
+		LEFT JOIN calendar_minmax d
+			ON b.pv_wc_id::numeric = d.refid::numeric;	
 
 END;
 $$ LANGUAGE plpgsql;
--- app/scripts/quorum_searchNearbyComponents.sql
+++ app/scripts/quorum_searchNearbyComponents.sql
@@ -1,23 +1,18 @@
-DROP FUNCTION fetchNearbyComponents(refid_arr numeric[]);
+DROP FUNCTION fetchNearbyComponentsNew(jsonb);
 
-CREATE OR REPLACE FUNCTION fetchNearbyComponents(refid_arr numeric[]) RETURNS TABLE (wellname varchar, refid numeric, pipelines text[]) AS 
+CREATE OR REPLACE FUNCTION fetchNearbyComponentsNew(req jsonb) RETURNS TABLE (refid numeric, pipelines text[]) AS 
 $$
 DECLARE
 	r numeric;
 BEGIN
-	RAISE NOTICE 'RefIDs = %', refid_arr;
+	RAISE NOTICE '% %', req, req -> 'productionWellList' -> 0;
 
-	FOREACH r IN ARRAY refid_arr LOOP
+	FOR r IN SELECT * FROM jsonb_array_elements(req -> 'productionWellList') LOOP
 		RAISE NOTICE 'r = %', r;
 		
-		RETURN QUERY SELECT
-	       		p.wellname,	
-			p.refid, 
-			string_to_array(p.items, '|') as pipelines 
-		FROM 
-			maps.nearby_pipelines p 
-		WHERE
-			p.refid::numeric = r::numeric;
+
+		RETURN QUERY SELECT p.refid, string_to_array(string_agg, '|') as pipelines  FROM maps.pipelines_in_2_miles p WHERE p.refid::numeric = r::numeric;
+
 	END LOOP;
 	
 	RETURN;
